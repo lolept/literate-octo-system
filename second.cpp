c85a65bb51f05a4ed122b9bc09065709884b9558
lolept <lolept230@gmail.com>
2023-10-17 12:46:13 +0300

    Added second.cpp

---
+++ second.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		std::cout << "Hello. I don`t know who are you\n";
+	} else {
+		std::cout << "Hi, " << argv[1] << std::endl;
+ 	}
+	return 0;
+}
